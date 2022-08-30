001c9ea060ef6b016cf0f7a071fbbebd4f3f485f
keveeen <105822487+keveeen@users.noreply.github.com>
2022-08-30 15:53:57 -0700

    commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { movies } from "../data";
 
+
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const moviesMap = movies.map((movie) => {
+      return (<div key={movie.title} >
+        {movie.title} 
+        {movie.time}
+        <li>
+          <ul>{movie.genres.map(genre => <li key={genre} >{genre}</li>)}</ul>
+        </li>
+      </div>)
+
+  })
+
+  console.log(moviesMap)
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {moviesMap}
+    </div>
+    
+    )
 }
 
 export default Movies;
